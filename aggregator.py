9f2fd0f3af496129ca07154c17d6fb983f4077c0
Oleksandr Zozulia <freemanlex@gmail.com>
2022-10-11 21:58:01 +0300

    modified:   aggregator.py 	modified:   task_description.py

--- aggregator.py
+++ aggregator.py
@@ -3,9 +3,8 @@ import init_js, referee, tests, python_3, js_node, task_description
 
 
 # Directory path
-directory_name = "C:\\Users\\ТЕХНОРАЙ\\Documents\\GitHub"
-if not os.path.exists(directory_name):
-    directory_name = "C:\\Users\\o.zozula\\Documents\\GitHub"
+if not os.path.exists((dir_name:="C:\\Users\\ТЕХНОРАЙ\\Documents\\GitHub")):
+    dir_name = "C:\\Users\\o.zozula\\Documents\\GitHub"
 # Mission name
 mission_name = "checkio-mission-remove-all-after"
 # js modifier
@@ -13,6 +12,7 @@ js_complex = True
 # py modifier
 py_iterable = True
 
+# commenting allows to migrate separate files
 files_to_convert = (
     # init_js,
     # python_3,
@@ -24,15 +24,15 @@ files_to_convert = (
 
 for file in files_to_convert:
     if file is js_node:
-        file.next_api(directory_name, mission_name, js_complex)
+        file.next_api(dir_name, mission_name, js_complex)
     elif file in (python_3, referee):
-        file.next_api(directory_name, mission_name, py_iterable)
+        file.next_api(dir_name, mission_name, py_iterable)
     else:
-        file.next_api(directory_name, mission_name)
+        file.next_api(dir_name, mission_name)
 
 # old init files deleting
 for file in ("js_node", "python_3", "python_27"):
     try:
-        os.remove(f"{directory_name}\\{mission_name}\\editor\\initial_code\\" + file)
+        os.remove(f"{dir_name}\\{mission_name}\\editor\\initial_code\\" + file)
     except:
         pass
--- task_description.py
+++ task_description.py
@@ -12,11 +12,11 @@ def task_desc_change(path: str) -> None:
         if "Example" in line:
             ex = ind
         elif all([ex, ind > ex,'{% if interpreter.slug' in line]):
-            task_start = ind
-        elif all([task_start, ind > task_start, '{% endif' in line]):
-            lines[task_start: ind + 1] = if_str
+            start = ind
+        elif all([start, ind > start, '{% endif' in line]):
+            lines[start: ind + 1] = if_str
         else:
-            lines[ind] = line.replace("interpreter.slug == \"js-node\"", "is_js")
+            lines[ind] = line.replace('interpreter.slug == "js-node"', 'is_js')
         
     with open(rf'{path}', mode='w', encoding='utf-8') as task_description: 
         task_description.write(''.join(lines))
@@ -25,25 +25,25 @@ def task_desc_change(path: str) -> None:
         index = path.find("\\info")
     print(f'{path[index:]} - OK')
 
-def next_api(directory_name: str, mission_name: str) -> None:
+def next_api(dir_name: str, mission_name: str) -> None:
     
     # Парсинг файла task_description.html
-    for parent, _, files in os.walk(f'{directory_name}\\{mission_name}'):
+    for parent, _, files in os.walk(f'{dir_name}\\{mission_name}'):
         if 'task_description.html' in files:
             task_desc_change(parent + '\\' + 'task_description.html')
 
     # create uk
-    if not os.path.exists((path_uk:=f"{directory_name}\\{mission_name}\\translations\\uk\\info")):
+    if not os.path.exists((path_uk:=f"{dir_name}\\{mission_name}\\translations\\uk\\info")):
         os.makedirs(path_uk)
         
     if not os.path.exists(path_uk + "\\task_description.html"):
 
-        with open(f"{directory_name}\\{mission_name}\\info\\task_description.html", 'r') as descr,\
+        with open(f"{dir_name}\\{mission_name}\\info\\task_description.html", 'r') as descr,\
              open(path_uk + "\\task_description.html", 'w') as descr_uk:
             descr_uk.write(descr.read())
 
     if not os.path.exists(path_uk + "\\task_short_description.html"):
 
-        with open(f"{directory_name}\\{mission_name}\\info\\task_short_description.html", 'r') as descr,\
+        with open(f"{dir_name}\\{mission_name}\\info\\task_short_description.html", 'r') as descr,\
              open(path_uk + "\\task_short_description.html", 'w') as descr_uk:
             descr_uk.write(descr.read())
